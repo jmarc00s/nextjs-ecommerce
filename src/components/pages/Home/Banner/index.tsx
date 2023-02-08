8df3bef7cdebccd4f0326b21b5395de3554582c2
João Marcos <jmarcossferreira@hotmail.com>
2023-02-08 08:29:06 -0300

    Improve folder strucure

---
+++ src/components/pages/Home/Banner/index.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/Ui";
+import Image from "next/image";
+import React from "react";
+
+export const HomeBanner = () => (
+  <div className="w-full flex justify-around items-center p-16 bg-base-200">
+    <div>
+      <h1 className="text-3xl font-bold">Next13 + FakeStoreApi</h1>
+      <p className="pt-2 pb-8">
+        E-commerce app to test some new features of Next13.
+      </p>
+      <Button rounded variant="primary">
+        Start now
+      </Button>
+    </div>
+
+    <Image
+      src="/images/web-shopping.svg"
+      alt="window shopping"
+      width="200"
+      height="200"
+    />
+  </div>
+);
