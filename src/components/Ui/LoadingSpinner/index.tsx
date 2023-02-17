f46d1ece65504fb6c960ea849885af15a0dbbcc4
João Marcos <jmarcossferreira@hotmail.com>
2023-02-17 08:11:09 -0300

    Change spinner to primary

--- src/components/Ui/LoadingSpinner/index.tsx
+++ src/components/Ui/LoadingSpinner/index.tsx
@@ -5,7 +5,7 @@ export const LoadingSpinner = () => {
     <div role="status">
       <svg
         aria-hidden="true"
-        className="w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-primary"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
